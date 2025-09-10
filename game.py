c4a4c718af4589b819d0d03567529ca60c91cf31
holistudio <daniel.bin.lu@gmail.com>
2025-09-09 21:04:37 -0400

    Update game.py
    Display empty board to the terminal

--- game.py
+++ game.py
@@ -1,5 +1,22 @@
-def main():
-    print("TIC-TAC-TOE!")
+class Board(object):
+    def __init__(self):
+        self.board = [[" "," "," "],[" "," "," "],[" "," "," "]]
+    def display(self):
+        """
+        Display the board to the terminal
+        """
+        for i,row in enumerate(self.board):
+            row_disp = ("|").join(row)
+            print(row_disp)
+            if i < 2:
+                print("-----")
+        return
     
+
+def main():
+    board = Board()
+    print("TIC-TAC-TOE!")
+    board.display()
+
 if __name__ == "__main__":
     main()
